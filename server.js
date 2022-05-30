65b75a142d93936123308e5b88e0fc0d2777bd9e
Your name here <kiranpv903@gmail.com>
2022-05-31 00:11:40 +0530

    security packages added

--- server.js
+++ server.js
@@ -2,28 +2,26 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+require('dotenv').config();
 
 
 
 // extra security packages
-// const helmet = require('helmet');
-// const cors = require('cors');
-// const xssClean = require('xss-clean');
-// const expressRateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const cors = require('cors');
+const xssClean = require('xss-clean');
+const expressRateLimit = require('express-rate-limit');
 
-// app.set('trust proxy', 1)
-// app.use(expressRateLimit({
-// 	windowMs: 15 * 60 * 1000, // 15 minutes
-// 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-// 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-// }))
-// app.use(helmet());
-// app.use(cors());
-// app.use(xssClean());
+app.set('trust proxy', 1)
+app.use(expressRateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
+app.use(helmet());
+app.use(cors());
+app.use(xssClean());
 
-app.use(express.urlencoded({extended: false}))
-require('dotenv').config();
 
 
 
